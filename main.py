e22d7c37bf3d4b4009ee8622876e3a7a7f3057de
achilles <izanloomoha@gmail.com>
2024-10-03 05:52:26 +0330

    fix image size and add comment

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import cv2
 
+# fucntion for convert 0-255 number to ascii
 def to_ascii(n):
     alpha = "^\”,:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
     asciiIndex = int((len(alpha) - 1) * (n / 255.0))
-    return alpha[asciiIndex]    
+    return alpha[asciiIndex]
 
-
+# open image file
 img = cv2.imread("./s.png", cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, (img.shape.x // 3, img.shape.y // 3))
 
+# get image width, height
 x, y = img.shape
 
+# resize image to smaller
+img = cv2.resize(img, (x // 6, y // 6))
+
+#get new image width, height after resizing
+x, y = img.shape
+
+# loop on image pixel and convert color number to ascii
 for i in range(x):
-    
     for j in range(y):
-        
         color_number = img[i][j]
         print(to_ascii(color_number), end=" ")
-    
+
     print()
